56fd96c232594c28b0bc2fb59c758474958bc9c2
ahxxm <i@ahxxm.com>
2016-01-16 20:22:26 +0800

    more settings

--- settings.py
+++ settings.py
@@ -2,4 +2,13 @@ import os
 
 DB_HOST = os.environ.get("DB_HOST", "mongo")
 DB_PORT = os.environ.get("DB_PORT", 27017)
+SESSION_FILENAME = "session.p"
 
+# Only z_c0 is required
+COOKIES = 'z_c0="cookiecontent"'
+
+# Go through this amount of users' answers.
+EXPLORE_USER_COUNT = 200
+EXPLORE_QUESTION_COUNT = 1000
+
+CONCURRENCY = 10
