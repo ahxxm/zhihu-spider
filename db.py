0c0eeccb9220620be6ff53282cbaa4eb0b15f365
ahxxm <i@ahxxm.com>
2016-01-18 20:38:29 +0800

    return cursor instead of result, replace deprecated method

--- db.py
+++ db.py
@@ -1,8 +1,8 @@
-from pymongo import MongoClient
+import pymongo
 
 from settings import DB_PORT, DB_HOST
 
-client = MongoClient(DB_HOST, DB_PORT)
+client = pymongo.MongoClient(DB_HOST, DB_PORT)
 db_client = client.Zhihu
 
 
@@ -13,40 +13,38 @@ class FLAG:
     IN_USE = 2
 
 
-def get_user_list(db, user_count) -> list:
-    users_list = list(db.users.find({'touched': FLAG.UNTOUCHED}).limit(user_count))
-    users_list = [str(user['user_id']) for user in users_list]
-    return users_list
+def get_user_cursor(db, count) -> pymongo.cursor.Cursor:
+    cursor = db.questions.find({'touched': FLAG.UNTOUCHED}).limit(count)
+    return cursor
 
 
-def get_untouched_questions(db, count):
-    questions_list = list(db.questions.find({'touched': FLAG.UNTOUCHED}).limit(count))
-    question_list = [str(question['question_id']) for question in questions_list]
-    return question_list
+def get_untouched_question_cursor(db, count) -> pymongo.cursor.Cursor:
+    cursor = db.questions.find({'touched': FLAG.UNTOUCHED}).limit(count)
+    return cursor
 
 
 def insert_new_question(db, question_id):
     if db.questions.find({'question_id': question_id}).count() == 0:
-        db.questions.save({'question_id': question_id, 'touched': FLAG.UNTOUCHED})
+        db.questions.insert_one({'question_id': question_id, 'touched': FLAG.UNTOUCHED})
 
 
 def change_question_status(db, question_id, status):
-    db.questions.update({'question_id': int(question_id)}, {'$set': {'touched': status}})
+    db.questions.update_one({'question_id': int(question_id)}, {'$set': {'touched': status}})
 
 
 def insert_new_user(db, user_id):
     if db.users.find({'user_id': user_id}).count() == 0:
-        db.users.save({'user_id': user_id, 'touched': FLAG.UNTOUCHED})
+        db.users.insert_one({'user_id': user_id, 'touched': FLAG.UNTOUCHED})
 
 
 def change_user_status(db, user_id: str, status):
-    db.users.update({'user_id': user_id},{'$set':{'touched': status}})
+    db.users.update_one({'user_id': user_id}, {'$set':{'touched': status}})
 
 
 def insert_answer(db, answer_id, author, question_id, comments_count, content):
     if db.answers.find({'answer_id': answer_id}).count() == 0:
-        db.answers.save({'answer_id': answer_id,
-                         'author': author,
-                         'relate_question': question_id,
-                         'comments_count': comments_count,
-                         'content': content})
+        db.answers.insert_one({'answer_id': answer_id,
+                               'author': author,
+                               'relate_question': question_id,
+                               'comments_count': comments_count,
+                               'content': content})
