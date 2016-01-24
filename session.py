2d130216d3ddd5871c28e95d677b0ff284e044fa
ahxxm <i@ahxxm.com>
2016-01-24 17:53:49 +0800

    use aiohttp session

--- session.py
+++ session.py
@@ -1,6 +1,6 @@
 import logging
 import pickle
-import requests
+import aiohttp
 import os
 
 from settings import SESSION_FILENAME, COOKIE_KEY, COOKIE_VALUE, CHROME_UA
@@ -9,11 +9,12 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 
-def validate_session(session: requests.session) -> bool:
+def validate_session(session: aiohttp.ClientSession) -> bool:
     # Validate by visit settings, anonymous user will be redirected
     # to login page.
     settings_url = "https://www.zhihu.com/settings/profile"
-    verify_rsp = session.get(settings_url)
+    verify_rsp = yield from session.get(settings_url)
+    verify_rsp.close()
 
     if not (verify_rsp.url == settings_url):
         obsolete_session_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
@@ -26,24 +27,32 @@ def validate_session(session: requests.session) -> bool:
     return True
 
 
-def get_or_create_session() -> requests.session:
+def get_or_create_session() -> aiohttp.ClientSession:
     try:
         with open(SESSION_FILENAME, "rb") as session_dump:
             session = pickle.load(session_dump)
             log.debug("Session loaded for user")
 
     except FileNotFoundError:
-        session = requests.session()
+        # Request Session
+        # session = requests.session()
+        # session.headers["User-Agent"] = CHROME_UA
+        # session.cookies.update({COOKIE_KEY: COOKIE_VALUE.strip()})
 
+        # Aiohttp session
         # mount cookies and UA
-        session.cookies.update({COOKIE_KEY: COOKIE_VALUE.strip()})
-        session.headers["User-Agent"] = CHROME_UA
+        connector = aiohttp.TCPConnector(conn_timeout=5, keepalive_timeout=10)
+        session = aiohttp.ClientSession(headers={"User-Agent": CHROME_UA},
+                                        connector=connector)
+        session.cookies[COOKIE_KEY] = COOKIE_VALUE
 
         # update cookies by visit website
         session.get("http://www.zhihu.com")
 
         validate_session(session)
-        with open(SESSION_FILENAME, "wb") as session_dump:
-            pickle.dump(session, session_dump)
+
+        # Aiohttp session can't be serialized.
+        # with open(SESSION_FILENAME, "wb") as session_dump:
+        #     pickle.dump(session, session_dump)
 
     return session
